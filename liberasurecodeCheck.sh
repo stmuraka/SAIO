26b354172e2a0078957edde022a4b658dddce8da
Shaun Murakami <stmuraka@us.ibm.com>
2017-04-10 16:14:31 -0700

    remove bash unset flag; add more output comments

--- liberasurecodeCheck.sh
+++ liberasurecodeCheck.sh
@@ -2,7 +2,7 @@
 
 # Checks to make sure liberasurecode is working before starting SAIO
 
-set -euo pipefail
+set -eo pipefail
 
 progress() {
     local pid=${1}
@@ -15,10 +15,11 @@ progress() {
 cd ${LIBERASURECODE_DIR} || { echo "ERROR: ${LIBERASURECODE_DIR} not found"; exit 1; }
 
 echo "Checking liberasurecode..."
-sleep 1
+sleep 3
 if ! make test; then
-    echo "test failed"
-    echo "Need to reconfigure liberasurecode"
+    echo ""
+    echo "liberasurecode test failed... reconfiguring liberasurecode."
+    echo ""
     echo -n "Preparing for build."
     sudo make clean >/dev/null 2>&1 || { echo "ERROR: failed to clean liberasurecode"; exit 1; }
     progress ${!}
@@ -27,14 +28,19 @@ if ! make test; then
     sudo ./configure >/dev/null 2>&1 || { echo "ERROR: failed to configure liberasurecode"; exit 1; }
     progress ${!}
     echo "ready"
+    echo ""
     echo -n "Compiling liberasurecode."
     sudo make >/dev/null 2>&1 || { echo "ERROR: failed to clean liberasurecode"; exit 1; } &
     progress ${!}
     echo "done"
-    echo "Testing liberasurecode."
-    sudo make test || { echo "ERROR: liberasurecode tests failed"; exit 1; } &
-    progress ${!}
-    echo "OK"
+    echo ""
+    echo "Testing liberasurecode..."
+    echo ""
+    sleep 3
+    sudo make test || { echo "ERROR: liberasurecode tests failed"; exit 1; }
+    echo ""
+    echo "liberasurecode tests passed"
+    echo ""
     echo -n "Installing liberasurecode."
     sudo make install >/dev/null 2>&1 || { echo "ERROR: failed to install liberasurecode"; exit 1; } &
     progress ${!}
